8b5bea94356621475aa322275198345f6950b000
drummingbird <notionsincognito@gmail.com>
2016-07-11 11:49:08 +1200

    Delete obsoleted modules.

--- mqo_exercise_sale/models/product.py
+++
@@ -1,9 +0,0 @@
-from openerp import models, fields, api
-
-class product(models.Model):
-    _name = 'product.template'
-    _inherit = 'product.template'
-    
-    hasbundles = fields.Boolean(string="Is an MQO exercise product?")
-    bundles = fields.Many2many('mqo.bundle', relation='mqo_bundle_product_relation', string="Exercise bundle")
-
