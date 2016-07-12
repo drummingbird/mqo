36b6d41522043c74e77c4e3fe639953e39fc32ae
drummingbird <notionsincognito@gmail.com>
2016-07-12 12:26:25 +1200

    [FIX] Allowed programs to be listed in sitemap.xml and minor improvement to css.

--- mqo_program/controllers/main.py
+++ mqo_program/controllers/main.py
@@ -7,17 +7,31 @@ from openerp.http import request
 from openerp.tools.translate import _
 
 class ProgramsList(http.Controller):
-    @http.route('/programs/<string(maxlength=15):category>', type='http', auth='public', website=True)
+    @http.route('/programs/all', type='http', auth='public', website=True)
+    def listAllPrograms(self):
+        return self.listPrograms('all')
+
+    @http.route('/programs/individuals', type='http', auth='public', website=True)
+    def listIndividualPrograms(self):
+        return self.listPrograms('individuals')
+    
+    @http.route('/programs/organisations', type='http', auth='public', website=True)
+    def listOrganisationalPrograms(self):
+        return self.listPrograms('organisations')
+
+   
     def listPrograms(self, category):
         if category=="all": 
             domain = []
             listname = "All programs"
-        if category=="individuals":
+        elif category=="individuals":
             domain = [('individual','=','true')]
             listname = "Programs for individuals"
-        if category=="organisations":
+        elif category=="organisations":
             domain = [('organisational','=','true')]
             listname = "Programs for organisations"
+        else:
+            return request.website.render('website.404');
         programs = request.env['mqo.program'].search(domain)
         if programs:
             return request.website.render('mqo_program.list', {
